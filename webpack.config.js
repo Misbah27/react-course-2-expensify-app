02a55c94db7ef1ad89233e3f71d08f270cb31d63
Mohd Misbahuddin <misbahud@amazon.com>
2020-04-26 16:27:01 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+module.exports = {
+    entry:  './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env','react'],
+                    plugins: ['transform-class-properties', 'transform-object-rest-spread']
+                }
+            }
+        },{
+            test:/\.s?css$/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
+        }]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        historyApiFallback: true
+    },
+    devtool: 'source-map'
+}
