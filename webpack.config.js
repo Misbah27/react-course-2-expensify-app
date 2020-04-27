32a459ac485fa9d3af9d390c22c2a149eb7b8ffc
Mohd Misbahuddin <misbahud@amazon.com>
2020-04-27 05:51:09 +0530

    setup production

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,35 @@
 const path = require('path')
 
-module.exports = {
-    entry:  './src/app.js',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['env','react'],
-                    plugins: ['transform-class-properties', 'transform-object-rest-spread']
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    
+    return {
+        entry:  './src/app.js',
+        output: {
+            path: path.resolve(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env','react'],
+                        plugins: ['transform-class-properties', 'transform-object-rest-spread']
+                    }
                 }
-            }
-        },{
-            test:/\.s?css$/,
-            use: ['style-loader', 'css-loader', 'sass-loader']
-        }]
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, 'public'),
-        historyApiFallback: true
-    },
-    devtool: 'source-map'
+            },{
+                test:/\.s?css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }]
+        },
+        devServer: {
+            contentBase: path.resolve(__dirname, 'public'),
+            historyApiFallback: true
+        },
+        devtool: 'source-map'
+    }
 }
+
