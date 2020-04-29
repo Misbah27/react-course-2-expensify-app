d7a038563a37bfb5155d9f0ce53a4334668f60a9
Mohd Misbahuddin <misbahud@amazon.com>
2020-04-30 00:37:45 +0530

    formats

--- webpack.config.js
+++ webpack.config.js
@@ -8,14 +8,20 @@ module.exports = (env) => {
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
     module: {
       rules: [{
-        loader: 'babel-loader',
         test: /\.js$/,
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['env','react'],
+                plugins: ['transform-class-properties', 'transform-object-rest-spread']
+            }
+        }
       }, {
         test: /\.s?css$/,
         use: CSSExtract.extract({
@@ -42,7 +48,8 @@ module.exports = (env) => {
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
-      historyApiFallback: true
+      historyApiFallback: true,
+      publicPath: '/dist/'
     }
   };
 };
