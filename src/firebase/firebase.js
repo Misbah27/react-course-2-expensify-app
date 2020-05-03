6eabe571c0d9fdbf0200fcd5d8c4f7136ce6fe9c
Mohd Misbahuddin <misbahud@amazon.com>
2020-05-03 17:05:08 +0530

    final push

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,103 +1,130 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
-    appId: process.env.FIREBASE_APPID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
-}
-
-
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
 
 firebase.initializeApp(config);
 
 const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-export {firebase, database as default};
+export { firebase, googleAuthProvider, database as default };
 
-// database.ref('expenses').on('value', (snapshot) => {
-//     const expenses = [];
-//     snapshot.forEach((childSnapshot) => {
-//         expenses.push({
-//             id: childSnapshot.key,
-//             ...childSnapshot.val()
-//         })
-//     })
+// // child_removed
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
 
-//     console.log(expenses);
-    
-// })
+// // child_changed
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
 
-// // database.ref('expenses').push({
-// //     description: 'Gum',
-// //     note: '',
-// //     amount: 195,
-// //     createdAt: 0
-// // })
+// // child_added
+// database.ref('expenses').on('child_added', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
 
-// // database.ref('expenses').push({
-// //     description: 'Masala',
-// //     note: '',
-// //     amount: 200,
-// //     createdAt: 0
-// // })
+// // database.ref('expenses')
+// //   .once('value')
+// //   .then((snapshot) => {
+// //     const expenses = [];
+
+// //     snapshot.forEach((childSnapshot) => {
+// //       expenses.push({
+// //         id: childSnapshot.key,
+// //         ...childSnapshot.val()
+// //       });
+// //     });
+
+// //     console.log(expenses);
+// //   });
+
+// // database.ref('expenses').on('value', (snapshot) => {
+// //   const expenses = [];
+
+// //   snapshot.forEach((childSnapshot) => {
+// //     expenses.push({
+// //       id: childSnapshot.key,
+// //       ...childSnapshot.val()
+// //     });
+// //   });
+
+// //   console.log(expenses);
+// // });
+
+// database.ref('expenses').push({
+//   description: 'Rent',
+//   note: '',
+//   amount: 109500,
+//   createdAt: 976123498763
+// });
+
+
+
+
+
+
+// // database.ref('notes/-Krll52aVDQ3X6dOtmS7').remove();
+
+// // database.ref('notes').push({
+// //   title: 'Course Topics',
+// //   body: 'React Native, Angular, Python'
+// // });
 
 // // database.ref().on('value', (snapshot) => {
+// //   const val = snapshot.val();
+// //   console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+// // })
+
+// // Setup data sub -> Andrew is a Software Developer at Amazon.
+
+// // Change the data and make sure it reprints
+
+// // database.ref('location/city')
+// //   .once('value')
+// //   .then((snapshot) => {
 // //     const val = snapshot.val();
-// //     console.log(`${val.name} is ${val.job.title} at ${val.job.company}`);
-    
-// // })
+// //     console.log(val);
+// //   })
+// //   .catch((e) => {
+// //     console.log('Error fetching data', e);
+// //   });
 
-// // firebase.database().ref().set({
-// //     name: 'Mohd Misbahuddin',
-// //     age:26,
-// //     job:{
-// //         title: 'Web developement engineer',
-// //         company: 'Amazon'
-// //     },
-// //     level: 5,
-// //     location: {
-// //         city: 'Hyderabad',
-// //         country: 'India'
-// //     }
+// // database.ref().set({
+// //   name: 'Andrew Mead',
+// //   age: 26,
+// //   stressLevel: 6,
+// //   job: {
+// //     title: 'Software developer',
+// //     company: 'Google'
+// //   },
+// //   location: {
+// //     city: 'Philadelphia',
+// //     country: 'United States'
+// //   }
 // // }).then(() => {
-// //     console.log('Data saved succesfully'); 
+// //   console.log('Data is saved!');
 // // }).catch((e) => {
-// //     console.log('Error is: ', e);
-// // })
+// //   console.log('This failed.', e);
+// // });
 
+// // database.ref().update({
+// //   stressLevel: 9,
+// //   'job/company': 'Amazon',
+// //   'location/city': 'Seattle'
+// // });
 
-// // firebase.database().ref().update({
-// //     'job/title': 'Web developement Manager',
-// //     level: 6,
-// //     'location/city': 'Banglore'
-// // })
-
-
-// // <!-- The core Firebase JS SDK is always required and must be listed first -->
-// // <script src="https://www.gstatic.com/firebasejs/7.14.2/firebase-app.js"></script>
-
-// // <!-- TODO: Add SDKs for Firebase products that you want to use
-// //      https://firebase.google.com/docs/web/setup#available-libraries -->
-// // <script src="https://www.gstatic.com/firebasejs/7.14.2/firebase-analytics.js"></script>
-
-// // <script>
-// //   // Your web app's Firebase configuration
-// //   var firebaseConfig = {
-// //     apiKey: "AIzaSyB-vgL0xHPql-k4o783VRv_3A1rOPHI7Hk",
-// //     authDomain: "expensify-4b53d.firebaseapp.com",
-// //     databaseURL: "https://expensify-4b53d.firebaseio.com",
-// //     projectId: "expensify-4b53d",
-// //     storageBucket: "expensify-4b53d.appspot.com",
-// //     messagingSenderId: "1042453633505",
-// //     appId: "1:1042453633505:web:b96b0c7a76561c3e7fdaa0",
-// //     measurementId: "G-PRFSNBJ93D"
-// //   };
-// //   // Initialize Firebase
-// //   firebase.initializeApp(firebaseConfig);
-// //   firebase.analytics();
-// // </script>
+// // database.ref()
+// //   .remove()
+// //   .then(() => {
+// //     console.log('Data was removed');
+// //   }).catch((e) => {
+// //     console.log('Did not remove data', e);
+// //   });
